3c9ae6dcb184a84088b4c3bed9535312cf65091f
glarochelle <35748719+glarochelle@users.noreply.github.com>
2018-05-11 04:28:57 -0400

    Add files via upload

---
+++ script1000.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
+  
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
+});
